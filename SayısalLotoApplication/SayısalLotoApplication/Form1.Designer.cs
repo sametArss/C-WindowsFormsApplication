bdd6bcea37110cb89728706c18f24d9a62604221
sametArss <154438926+sametArss@users.noreply.github.com>
2023-12-21 11:13:16 +0300

    Add files via upload

---
+++ SayısalLotoApplication/SayısalLotoApplication/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace SayısalLotoApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            label5 = new Label();
+            label6 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 16.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label1.Location = new Point(104, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(580, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Sayısal Loto Uygulamasına Hoşgeldiniz !!!";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial Narrow", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(138, 145);
+            label2.Name = "label2";
+            label2.Size = new Size(174, 24);
+            label2.TabIndex = 1;
+            label2.Text = "Adınız ve Soyadınız : ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial Narrow", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(237, 188);
+            label3.Name = "label3";
+            label3.Size = new Size(75, 24);
+            label3.TabIndex = 2;
+            label3.Text = "Yaşınız :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Arial Narrow", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(189, 229);
+            label4.Name = "label4";
+            label4.Size = new Size(123, 24);
+            label4.TabIndex = 3;
+            label4.Text = "Bakiyeniz (TL) :";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(339, 145);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(204, 27);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(339, 188);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(204, 27);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(339, 229);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(204, 27);
+            textBox3.TabIndex = 6;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(210, 286);
+            button1.Name = "button1";
+            button1.Size = new Size(385, 37);
+            button1.TabIndex = 7;
+            button1.Text = "Uygulamaya Git";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.ForeColor = Color.Red;
+            label5.Location = new Point(354, 387);
+            label5.Name = "label5";
+            label5.Size = new Size(395, 20);
+            label5.TabIndex = 8;
+            label5.Text = "Not : Lütfen yukardaki alanları eksiksiz şekilde doldurunuz .";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 7F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(561, 241);
+            label6.Name = "label6";
+            label6.Size = new Size(91, 15);
+            label6.TabIndex = 9;
+            label6.Text = "Min.Tutar : 50TL";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.PeachPuff;
+            ClientSize = new Size(765, 416);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+        private Label label5;
+        private Label label6;
+    }
+}
---
+++ SayısalLotoApplication/SayısalLotoApplication/Form2.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SayısalLotoApplication
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public int bakiye;
+        int sayac = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+            int s1, s2, s3, s4, s5;
+
+
+
+            if (bakiye < 50)
+            {
+                MessageBox.Show("Lütfen Para Miktarını Yükseltiniz ...", "Bilgilendirme Kutusu ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+            }
+
+
+            s1 = rnd.Next(0, 10);
+            s2 = rnd.Next(0, 10);
+            s3 = rnd.Next(0, 10);
+            s4 = rnd.Next(0, 10);
+            s5 = rnd.Next(0, 10);
+
+            label3.Text = s1.ToString();
+            label4.Text = s2.ToString();
+            label5.Text = s3.ToString();
+            label6.Text = s4.ToString();
+            label7.Text = s5.ToString();
+
+
+            // sayi1
+            if (textBox1.Text == label3.Text)
+            {
+                textBox1.BackColor = Color.Green;
+                sayac++;
+            }
+            else { textBox1.BackColor = Color.Red; }
+
+            // sayi2
+            if (textBox2.Text == label4.Text)
+            {
+                textBox2.BackColor = Color.Green;
+                sayac++;
+            }
+            else { textBox2.BackColor = Color.Red; }
+
+            // sayi3
+            if (textBox3.Text == label5.Text)
+            {
+                textBox3.BackColor = Color.Green;
+                sayac++;
+            }
+            else { textBox3.BackColor = Color.Red; }
+
+            // sayi4
+            if (textBox4.Text == label6.Text)
+            {
+                textBox4.BackColor = Color.Green;
+                sayac++;
+            }
+            else { textBox4.BackColor = Color.Red; }
+
+            // sayi5
+            if (textBox5.Text == label7.Text)
+            {
+                textBox5.BackColor = Color.Green;
+                sayac++;
+            }
+            else { textBox5.BackColor = Color.Red; }
+
+            bakiye = (bakiye - 50) + (sayac * 20);
+            label8.Text = bakiye.ToString() + " TL";
+
+
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Başarılı Bir Şekilde Çıkış Yapılmıştır.", "Çıkış Yapıldı", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+
+            textBox1.BackColor = Color.White;
+            textBox2.BackColor = Color.White;
+            textBox3.BackColor = Color.White;
+            textBox4.BackColor = Color.White;
+            textBox5.BackColor = Color.White;
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
